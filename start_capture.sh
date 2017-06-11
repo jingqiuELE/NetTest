5d7c4a47e25f71d6c530deffd3b8736896e989b4
Jing Qiu <aqiu0720@gmail.com>
2017-06-11 13:16:20 +0800

    Fix the incorrect docker image name.

--- start_capture.sh
+++ start_capture.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 MININET_NAME="mininet"
-docker run --rm --net=container:$MININET_NAME docker-tcpdump -l -i s2-eth1 --immediate-mode -w - | wireshark -k -i - &
-docker run --rm --net=container:$MININET_NAME docker-tcpdump -l -i s3-eth1 --immediate-mode -w - | wireshark -k -i - &
+docker run --rm --net=container:$MININET_NAME jingqiu/docker-tcpdump -l -i s2-eth1 --immediate-mode -w - | wireshark -k -i - &
+docker run --rm --net=container:$MININET_NAME jingqiu/docker-tcpdump -l -i s3-eth1 --immediate-mode -w - | wireshark -k -i - &
 
